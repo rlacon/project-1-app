e87cacb13413b80db19cae1cad2e8c2fb9788fa7
Dan Beccaria <beccdaniel@gmail.com>
2019-07-12 22:31:24 -0400

    attempting to get meriam-webster api to work, running into cors issues

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,9 @@
 $(document).ready(function (){
-    
+    $.support.cors = true
+    $.ajax({
+        query:"https://dictionaryapi.com/api/v3/references/collegiate/json/test?key=badf0fb9-2111-4efc-99f2-e6ac5ada80bd",
+        method: "GET"
+    }).then(function (response){
+        console.log(response)
+    })
 })
