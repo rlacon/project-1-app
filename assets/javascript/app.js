e546e5db0520e809121b37dedb55e06e64d3f529
Dan Beccaria <beccdaniel@gmail.com>
2019-07-13 06:49:49 -0400

    added search term to the meriam-webster api query

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,7 @@
 $(document).ready(function (){
     
     $.ajax({
-        query:"https://dictionaryapi.com/api/v3/references/collegiate/json/test?q=headword&key=badf0fb9-2111-4efc-99f2-e6ac5ada80bd",
+        query:"http://dictionaryapi.com/api/v3/references/collegiate/json/voluminous?&key=badf0fb9-2111-4efc-99f2-e6ac5ada80bd",
         method: "GET"
     }).then(function (response){
         console.log(response)
