414af15f0b318a94e8a954987b8cd4adaa4e84d7
Dan Beccaria <beccdaniel@gmail.com>
2019-07-13 00:17:28 -0400

    deleted $.support.cors=true

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 $(document).ready(function (){
-    $.support.cors = true
+    
     $.ajax({
         query:"https://dictionaryapi.com/api/v3/references/collegiate/json/test?key=badf0fb9-2111-4efc-99f2-e6ac5ada80bd",
         method: "GET"
