0d852d52ab52b50bc39a67ef7f936016f2b9033f
Dan Beccaria <beccdaniel@gmail.com>
2019-07-13 07:25:22 -0400

    added dictionary search button user can take advantage of to look up definitions of words

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,24 @@
 $(document).ready(function (){
     
+     $("#usersubmit").on('click', function () {
+            event.preventDefault()
+            let userSearch = $('#usersearch').val().trim().toLowerCase()
+            //Clearing whatever user typed
+            $('#usersearch').val('')
+
+            let query = "http://dictionaryapi.com/api/v3/references/collegiate/json/"+userSearch+"?&key=badf0fb9-2111-4efc-99f2-e6ac5ada80bd"
+       
+        
+    
+    
     $.ajax({
-        query:"http://dictionaryapi.com/api/v3/references/collegiate/json/voluminous?&key=badf0fb9-2111-4efc-99f2-e6ac5ada80bd",
+        url:query,
         method: "GET"
     }).then(function (response){
         console.log(response)
-    })
+        let def = response[0].shortdef.map(item => item )
+        console.log(def)
+       $("#definition").append('<h5>' + def + '</h5>')
+    }) 
+})
 })
