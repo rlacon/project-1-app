71b6f7f8bb23eb9d718a87e7cf7a6b7fd96047f0
Dan Beccaria <beccdaniel@gmail.com>
2019-07-14 17:42:38 -0400

    dictionary search bar now creates a list with the defiitions, ideally so someone could refer to it as they write their story

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,27 +13,39 @@ firebase.initializeApp(config);
 
 //let database = firebase.database()
 
-
+ 
+      
 
 
 $(document).ready(function () {
+    let counter  = 0;
 
-    $("#userD").on('click', function () {
+    $("#userD").on('click', () => {
+        counter++
+
         event.preventDefault()
-        let userDefine = $('#userDefine').val().trim().toLowerCase()
+        let item = $('#userDefine').val().trim().toLowerCase()
+        
 
-
-        let query = "https://dictionaryapi.com/api/v3/references/collegiate/json/" + userDefine + "?&key=badf0fb9-2111-4efc-99f2-e6ac5ada80bd"
-      
-        $('#definethes').append('<p class=border border-success id=definition><strong></strong></p>')
-        $('#definition').before('<h3 id=wordDef>' + userDefine + '</h3>')
+        let query = "https://dictionaryapi.com/api/v3/references/collegiate/json/" + item + "?&key=badf0fb9-2111-4efc-99f2-e6ac5ada80bd"
+        //definethes
        
-        $('#wordDef').text(userDefine)
+
+        let list = $('#definitions').css({'font-size' : '24px'})
+        
+        list.append('<li id=def_' +counter+'>' + item + '</li>').css({ 'border': '1px solid black', 'list-style': 'none', 'font-weight':'bold'})
+        
         $('#userDefine').val('')
         
+        //$('#definethes').append('<p class=border border-success id=definition><strong></strong></p>')
+        //$('#definition').before('<h3 id=wordDef>' + userDefine + '</h3>')
+       
+        //$('#wordDef').remove(userDefine)
+        //$('#wordDef').append('<h3 id=wordType></h3>')
+        //$('#userDefine').val('')
         
-
-
+        
+       
         $.ajax({
             url: query,
             method: "GET"
@@ -41,18 +53,20 @@ $(document).ready(function () {
 
             let def = response[0].shortdef.map(item => item)
             let type = response[0].fl
+            $('#def_' + counter).append(': '+ def)
             console.log(def)
-            $('#wordDef').append(' : ' + type)
-            $('#definition').text(def.join(' '))
+            console.log(type)
+            //$('#wordType').replaceWith(' : ' + type)
+            //$('#definition').text(def.join(' '))
         })
     })
-    $('#userT').on('click', function () {
+    $('#userT').on('click', () => {
         event.preventDefault()
         let userThes = $('#userThes').val().trim().toLowerCase()
         
-        $('#definethes').append('<p class=border border-success id=synonym><strong></strong></p>')
-        $('#synonym').before('<h3 id=wordThes>' + userThes + '</h3>')
-        $('#userThes').val('')
+      //$('#definethes').append('<p class=border border-success id=synonym><strong></strong></p>')
+      //$('#synonym').before('<h3 id=wordThes>' + userThes + '</h3>')
+      //$('#userThes').val('')
       
         let query_2 = "https://dictionaryapi.com/api/v3/references/thesaurus/json/" + userThes + "?&key=16c11365-c317-4d39-aa7c-62632093e7ef"
 
@@ -63,11 +77,11 @@ $(document).ready(function () {
             console.log(response)
 
             let thes = response[0].meta.syns[0].map(item => item)
-
+            console.log(thes)
            
            
-            $('#synonym').text(thes.join(' , '))
-            $('#wordThes').append(' :synonyms')
+            //$('#synonym').text(thes.join(' , '))
+            //$('#wordThes').append(' :synonyms')
             
 
 
