f023ff042eaabdf98a77e0f41117e62ea753731e
Dan Beccaria <beccdaniel@gmail.com>
2019-07-16 10:48:58 -0400

    added genre display

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,15 @@
 $(document).ready(() => {
     
-  
+    console.log(moment().format("dddd"))
+    $('#day').text(moment().format('dddd'))
+    
 
+    
     getWordCount = (str) => {
         let regex = /\S+/g;
         let found = str.match(regex)
         console.log(found.length)
+        $('#data').text(found.length)
     }
 
 
@@ -24,17 +28,23 @@ $(document).ready(() => {
     let horror = new getGenre('Horror','A haunted mansion', 'A woman receives a message from someone who she thinks is still alive', 'Woman goes to investigate mysterious message', 'Woman encounters and confronts a great evil lving within the mansion')
     let scifi = new getGenre('Scifi','A derelict spaceship', 'An astronaut wakes up in stasis to find hes the lone survivor on a mining vessel','He discovers can alien force has massacred his crew and left him alive, but for what reason?','Discovers reason, conflict resolution')
     let mystery = new getGenre('Mystery','Rural America, 1930', 'A man who does not age seeks to find out why after being alive for more than 200 years', 'He discovers one of his family members was a witch and cursed him with immortality','Resolves the immortality and finally dies')
-
+    
     $('#action').on('click', () => {
         let title = $('#title').val().trim()
         localStorage.setItem(title, action.setting)
         getWordCount(title)
+
+
+       
+      
+
         loop(action)
     })
     $('#horror').on('click', () => {
         let title = $('#title').val().trim()
         localStorage.setItem(title, horror.setting)
         loop(horror)
+       
     })
     $('#scifi').on('click', () => {
         let title = $('#title').val().trim()
@@ -52,6 +62,10 @@ $(document).ready(() => {
     const values = Object.values(genre)
     for(const value of values) {
        console.log(value)
+       $('#loop-info-1').text('Setting: ' + values[1])
+       $('#loop-info-2').text('First Plot Point: ' + values[2])
+       $('#loop-info-3').text('Midpoint: ' + values[3])
+       $('#loop-info-4').text('Climax : ' + values[4])
    }
 }
 
@@ -64,9 +78,9 @@ $(document).ready(() => {
 
     //-------Ajax calls and events for Dictionary and Thesaurus
 
-let counter  = 0;
+
     $("#userD").on('click', () => {
-        counter++
+        
 
         event.preventDefault()
         let item = $('#userDefine').val().trim().toLowerCase()
@@ -76,9 +90,9 @@ let counter  = 0;
         //definethes
         //
 
-        let list = $('#definitions').css({'font-size' : '24px'})
+        let data = $('#definitions').css({'font-size' : '24px'})
         
-        list.append('<li id=def_' +counter+'>' + item + '</li>').css({ 'border': '1px solid black', 'list-style': 'none', 'font-weight':'bold', 'float':'left'})
+        data.append('<li id=def_' +counter+'>' + item + '</li>').css({ 'border': '1px solid black', 'data-style': 'none', 'font-weight':'bold', 'float':'left'})
         
         $('#userDefine').val('')
 
@@ -111,11 +125,13 @@ let counter  = 0;
     })
     $('#userT').on('click', () => {
         event.preventDefault()
-        counter++
+        
         let item = $('#userThes').val().trim().toLowerCase()
-        let list = $('#thesaurus').css({'font-size' : '24px'})
+        let data = $('#thesaurus').css({'font-size' : '24px'})
         $('#userThes').val('')
-        list.append('<li id=thes_'+counter+'>'+ item +'</li>').css({ 'border': '1px solid black', 'list-style': 'none', 'font-weight':'bold', 'float':'left'})
+        data.append('<p id=thes></p>')
+        $('#thes').text(item)
+        $('#thes').append('<p id=syn></p>')
       //$('#definethes').append('<p class=border border-success id=synonym><strong></strong></p>')
       //$('#synonym').before('<h3 id=wordThes>' + userThes + '</h3>')
       //$('#userThes').val('')
@@ -130,7 +146,8 @@ let counter  = 0;
 
             let thes = response[0].meta.syns[0].map(item => item)
             console.log(thes)
-           $('#thes_' + counter).append(': ' + thes)
+          $('#syn').text(thes)
+           
             
             //$('#synonym').text(thes.join(' , '))
             //$('#wordThes').append(' :synonyms')
